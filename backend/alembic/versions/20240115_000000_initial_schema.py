0f075e95d85efca5374b34c311eb7b14948c84dc
ianagora <ian@agorasconsulting.com>
2026-01-24 16:48:36 +0000

    🔧 FIX: Correct initial migration revision ID to 20240115_000000

--- backend/alembic/versions/20240115_000000_initial_schema.py
+++ backend/alembic/versions/20240115_000000_initial_schema.py
@@ -1,6 +1,6 @@
 """Initial database schema
 
-Revision ID: 001_initial
+Revision ID: 20240115_000000
 Revises: 
 Create Date: 2024-01-15 00:00:00.000000
 
@@ -12,7 +12,7 @@ import sqlalchemy as sa
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
-revision: str = '001_initial'
+revision: str = '20240115_000000'
 down_revision: Union[str, None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
